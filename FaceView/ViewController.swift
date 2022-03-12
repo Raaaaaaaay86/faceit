231a12ebae4820ee86afb904d5efb99b15e271f6
Ray Lin <mirror46258@gmail.com>
2022-03-12 15:20:12 +0800

    Initial Commit

---
+++ FaceView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceView
+//
+//  Created by 林家富 on 2022/3/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
