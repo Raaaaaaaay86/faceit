b1c65d7683835d26a489f45e960fb250043f867b
Ray Lin <mirror46258@gmail.com>
2022-03-13 10:29:45 +0800

    -

---
+++ FaceView/FacialExpression.swift
@@ -0,0 +1,43 @@
+//
+//  FacialExpression.swift
+//  FaceView
+//
+//  Created by 林家富 on 2022/3/13.
+//
+
+import Foundation
+
+struct FacialExpression {
+    enum Eyes: Int {
+        case open
+        case closed
+        case squinting
+    }
+    
+    enum Mouth: Int {
+        case frown
+        case smirk
+        case neutral
+        case grin
+        case smile
+        
+        var sadder: Mouth {
+            return Mouth(rawValue: rawValue - 1) ?? .frown
+        }
+        
+        var happier: Mouth {
+            return Mouth(rawValue: rawValue + 1) ?? .smile
+        }
+    }
+    
+    var sadder: FacialExpression {
+        return FacialExpression(eyes: self.eyes, mouth: self.mouth.sadder)
+    }
+    
+    var happier: FacialExpression {
+        return FacialExpression(eyes: self.eyes, mouth: self.mouth.happier)
+    }
+    
+    let eyes: Eyes
+    let mouth: Mouth
+}
--- FaceView/ViewController.swift
+++ FaceView/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    var expression = FacialExpression(eyes: .closed, mouth: .frown) {
+        didSet {
+            updateUI()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    private func updateUI() {
+        switch expression.eyes {
+            case .open:
+                faceView?.eyesOpen = true
+            case .closed:
+                faceView?.eyesOpen = false
+            case .squinting:
+                faceView?.eyesOpen = false
+        }
+        faceView?.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+    }
+    
+    private let mouthCurvatures = [
+        FacialExpression.Mouth.grin : 0.5,
+        FacialExpression.Mouth.frown: -1.0,
+        FacialExpression.Mouth.smile: 1.0,
+        FacialExpression.Mouth.smirk: 0.0,
+        FacialExpression.Mouth.neutral: -0.5,
+    ]
 }
-
